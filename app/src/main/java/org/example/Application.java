c6bfafdb90f53d850ec41f18b8ee96e2b77af16b
Tsuyoshi Higashijo <tsuyoshi.higashijo@bigtreetc.com>
2024-04-15 12:14:31 +0900

    new

---
+++ app/src/main/java/org/example/Application.java
@@ -0,0 +1,21 @@
+// src/main/java/com/example/exptapprunnerspringboot/Application.java
+
+package org.example;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        
+    }
+
+}
